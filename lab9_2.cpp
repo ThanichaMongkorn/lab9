7f8389eb3ccd000146aa77c5ec86a560de05e5c9
= <=>
2022-12-23 15:54:19 +0700

    lab9.2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,7 +3,27 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double x){
+
+    if(x>90){
+        return 'A';
+        
+    }
+    
+    if(x>75 ){
+        return 'B';
+    }
+    
+    if(x>60 ){
+        return 'C';
+    }
+    
+    if(x>45){
+        return 'D';
+    }else{
+        return 'F';
+    }
+}
 
 int main(){
 	//Input the number of students
@@ -17,9 +37,11 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline (cin,name[i]);
+		
+		
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
